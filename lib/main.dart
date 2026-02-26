358ce0b37c2572d8c18dcb694dec202da8fd220f
Mohamed Gamil <mohamedgamil06616@gmail.com>
2026-02-26 14:27:46 +0200

    Integrate Firebase into the project with configuration files and initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:skillbridge/core/routing/app_router.dart';
+import 'package:skillbridge/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const SkillBridge());
 }
 
