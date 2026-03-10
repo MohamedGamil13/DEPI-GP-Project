ee2344a64df22cd3fbe35029d72a8c6b22d6cc8e
Mohammed Khalid <hamada.mkhalid@gmail.com>
2026-03-11 01:37:50 +0200

    update home screen and add images

--- lib/core/utils/constants/app_images.dart
+++ lib/core/utils/constants/app_images.dart
@@ -1,4 +1,7 @@
 abstract class AppImages {
   static const String appLogo = 'assets/images/ServiMarket.png';
+  static const String laptop = 'assets/images/laptop.jpg';
+  static const String study = 'assets/images/study.jpg';
+  static const String home_clean = 'assets/images/home clean.jpg';
   AppImages._();
 }
