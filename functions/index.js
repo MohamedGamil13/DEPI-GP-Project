c5a254c5b27a2300733175de516bf3e8a5d12df8
yousef gabr <yousef.gabr03@gmail.com>
2026-06-28 21:08:16 +0300

    feat: Add Cloud Function to send FCM on new messages

---
+++ functions/index.js
@@ -0,0 +1,38 @@
+const { initializeApp } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
+const { getMessaging } = require('firebase-admin/messaging');
+const { onDocumentCreated } = require('firebase-functions/v2/firestore');
+
+initializeApp();
+
+exports.onNewMessage = onDocumentCreated(
+  'conversations/{conversationId}/messages/{messageId}',
+  async (event) => {
+    const msg = event.data.data();
+    const { conversationId } = event.params;
+
+    const db = getFirestore();
+
+    const [receiverSnap, senderSnap] = await Promise.all([
+      db.collection('usersMetaData').doc(msg.receiverId).get(),
+      db.collection('usersMetaData').doc(msg.senderId).get(),
+    ]);
+
+    const fcmToken = receiverSnap.data()?.fcmToken;
+    if (!fcmToken) return; // receiver hasn't granted notifications
+
+    const senderName = senderSnap.data()?.name ?? 'Someone';
+
+    await getMessaging().send({
+      token: fcmToken,
+      notification: {
+        title: senderName,
+        body: msg.text,
+      },
+      data: {
+        type: 'message',
+        conversationId,
+      },
+    });
+  },
+);
